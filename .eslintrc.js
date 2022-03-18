c4ad5843d6734d214bffe08cf854e7d8c57cffa4
Luciano Galvão <luciano_sdmf@hotmail.com>
2022-03-17 21:41:37 -0300

    Config file commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'linebreak-style': 0,
+    quotes: ['error', 'single'],
+    // we want to force semicolons
+    semi: ['error', 'always'],
+    // we use 2 spaces to indent our code
+    indent: ['error', 2],
+    // we want to avoid extraneous spaces
+    'no-multi-spaces': ['error'],
+    'class-methods-use-this': 'off',
+    'no-promise-executor-return': 'off',
+    camelcase: 'off',
+  },
+};
