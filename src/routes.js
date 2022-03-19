572dfa34db386a805a75f2114509c76691ed67b5
Luciano Galvão <luciano_sdmf@hotmail.com>
2022-03-18 21:25:46 -0300

    Routes add

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,22 @@
 const { Router } = require('express');
+const CategoryController = require('./app/controllers/CategoryController');
 
 const ContactController = require('./app/controllers/ContactController');
 
 const router = Router();
 
-router.get(
-  '/contacts',
-  (request, response, next) => {
-    request.appId = 'MeuAppID';
-    next();
-  },
-  ContactController.index,
-);
-
+// Contact Routes
+router.get('/contacts', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
-
+router.post('/contacts', ContactController.store);
+router.put('/contacts/:id', ContactController.update);
 router.delete('/contacts/:id', ContactController.delete);
 
-router.post('/contacts', ContactController.store);
-
-router.put('/contacts/:id', ContactController.update);
+// Category Routes
+router.get('/categories', CategoryController.index);
+router.get('/categories/:id', CategoryController.show);
+router.post('/categories', CategoryController.store);
+router.put('/categories/:id', CategoryController.update);
+router.delete('/categories/:id', CategoryController.delete);
 
 module.exports = router;
